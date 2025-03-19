91830bce19f7b04f072d9819c86e3a15449b0c81
David Janssens <fiammybe@gmail.com>
2025-03-19 11:41:55 +0100

    New translations common.php (Italian)

--- language/it/common.php
+++ language/it/common.php
@@ -12,81 +12,81 @@
 
 if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
 
-define("_CO_IMFAQ_READMORE", "Read more...");
+define("_CO_IMFAQ_READMORE", "Leggi tutto...");
 
 // faq
 define("_CO_IMFAQ_FAQ_FAQ_ID", "Id");
 define("_CO_IMFAQ_FAQ_FAQ_ID_DSC", " ");
-define("_CO_IMFAQ_FAQ_FAQ_QUESTION", "Question");
+define("_CO_IMFAQ_FAQ_FAQ_QUESTION", "Domanda");
 define("_CO_IMFAQ_FAQ_FAQ_QUESTION_DSC", " ");
-define("_CO_IMFAQ_FAQ_FAQ_MENUTITLE", "Title in menu");
+define("_CO_IMFAQ_FAQ_FAQ_MENUTITLE", "Titolo nel menu");
 define("_CO_IMFAQ_FAQ_FAQ_MENUTITLE_DSC", " ");
-define("_CO_IMFAQ_FAQ_FAQ_ANSWER", "Answer");
+define("_CO_IMFAQ_FAQ_FAQ_ANSWER", "Risposta");
 define("_CO_IMFAQ_FAQ_FAQ_ANSWER_DSC", " ");
 define("_CO_IMFAQ_FAQ_FAQ_UID", "Autore");
 define("_CO_IMFAQ_FAQ_FAQ_UID_DSC", " ");
 define("_CO_IMFAQ_FAQ_FAQ_CID", "Categoria");
 define("_CO_IMFAQ_FAQ_FAQ_CID_DSC", " ");
-define("_CO_IMFAQ_FAQ_FAQ_DIDUNO", "Did you know?");
-define("_CO_IMFAQ_FAQ_FAQ_DIDUNO_DSC", "This will be used in the Did You Know block.");
+define("_CO_IMFAQ_FAQ_FAQ_DIDUNO", "Lo sapevate?");
+define("_CO_IMFAQ_FAQ_FAQ_DIDUNO_DSC", "Questo verrà usato nel blocco Sapevi.");
 define("_CO_IMFAQ_FAQ_FAQ_STATUS", "Stato");
 define("_CO_IMFAQ_FAQ_FAQ_STATUS_DSC", " ");
-define("_CO_IMFAQ_FAQ_FAQ_PUBLISHED_DATE", "Published Date");
+define("_CO_IMFAQ_FAQ_FAQ_PUBLISHED_DATE", "Data Pubblicazione");
 define("_CO_IMFAQ_FAQ_FAQ_PUBLISHED_DATE_DSC", " ");
-define("_CO_IMFAQ_FAQ_FAQ_CANCOMMENT", "Can Comment?");
+define("_CO_IMFAQ_FAQ_FAQ_CANCOMMENT", "Può Commentare?");
 define("_CO_IMFAQ_FAQ_FAQ_CANCOMMENT_DSC", " ");
-define("_CO_IMFAQ_FAQ_FAQ_WEIGHT", "Weight");
+define("_CO_IMFAQ_FAQ_FAQ_WEIGHT", "Peso");
 define("_CO_IMFAQ_FAQ_FAQ_WEIGHT_DSC", " ");
-define("_CO_IMFAQ_FAQ_FAQ_ATTACHMENTS", "Attachments");
-define("_CO_IMFAQ_FAQ_FAQ_ATTACHMENTS_DSC", "Click the button \"add attachments\" to select and attach items to this faq");
-define("_CO_IMFAQ_FAQ_LEARNMORE", "To learn more, see the following NAIMA Publications:");
-define("_CO_IMFAQ_FAQ_FAQ_NOTIFYPUB", "Notify on approval?");
+define("_CO_IMFAQ_FAQ_FAQ_ATTACHMENTS", "Allegati");
+define("_CO_IMFAQ_FAQ_FAQ_ATTACHMENTS_DSC", "Fare clic sul pulsante \"Aggiungi allegati\" per selezionare e allegare gli elementi a questo faq");
+define("_CO_IMFAQ_FAQ_LEARNMORE", "Per saperne di più, consulta le seguenti pubblicazioni NAIMA:");
+define("_CO_IMFAQ_FAQ_FAQ_NOTIFYPUB", "Notificare l'approvazione?");
 
-define("_CO_IMFAQ_FAQ_STATUS_PUBLISHED", "Published");
+define("_CO_IMFAQ_FAQ_STATUS_PUBLISHED", "Pubblicato");
 define("_CO_IMFAQ_FAQ_STATUS_PENDING", "In attesa");
-define("_CO_IMFAQ_FAQ_STATUS_REJECTED", "Rejected");
+define("_CO_IMFAQ_FAQ_STATUS_REJECTED", "Respinto");
 define("_CO_IMFAQ_FAQ_STATUS_OFFLINE", "Offline");
 
-define("_CO_IMFAQ_FAQ_READ", "View Permission");
-define("_CO_IMFAQ_FAQ_READ_DSC", "Select wich groups will have view permission for this faq. This means that a user belonging to one of these groups will be able to view the faq when it is activated in the site.");
+define("_CO_IMFAQ_FAQ_READ", "Visualizza Permesso");
+define("_CO_IMFAQ_FAQ_READ_DSC", "Seleziona i gruppi wich avranno i permessi di visualizzazione per questo faq. Ciò significa che un utente appartenente a uno di questi gruppi sarà in grado di visualizzare il faq quando viene attivato nel sito.");
 
-define("_CO_IMFAQ_FAQ_INFO", "Published by %s on %s. (%u reads)");
-define("_CO_IMFAQ_FAQ_FROM_USER", "All contents of %s");
-define("_CO_IMFAQ_FAQ_COMMENTS_INFO", "%d comments");
-define("_CO_IMFAQ_FAQ_NO_COMMENT", "No comment");
+define("_CO_IMFAQ_FAQ_INFO", "Pubblicato da %s su %s. (%u leggi)");
+define("_CO_IMFAQ_FAQ_FROM_USER", "Tutti i contenuti di %s");
+define("_CO_IMFAQ_FAQ_COMMENTS_INFO", "%d commenti");
+define("_CO_IMFAQ_FAQ_NO_COMMENT", "Nessun commento");
 
-define("_CO_IMFAQ_SUBMITTED_FAQS", "<h1>User's submitted FAQs</h1>");
-define("_CO_IMFAQ_REQUESTED_FAQS", "<h1>User's requested FAQs</h1>");
+define("_CO_IMFAQ_SUBMITTED_FAQS", "<h1>Domande frequenti inviate dall'utente</h1>");
+define("_CO_IMFAQ_REQUESTED_FAQS", "<h1>Domande frequenti richieste dall'utente</h1>");
 
 // category
 define("_CO_IMFAQ_CATEGORY_CAT_ID", "Id");
 define("_CO_IMFAQ_CATEGORY_CAT_ID_DSC", " ");
 define("_CO_IMFAQ_CATEGORY_CAT_TITLE", "Titolo");
 define("_CO_IMFAQ_CATEGORY_CAT_TITLE_DSC", " ");
-define("_CO_IMFAQ_CATEGORY_CAT_MENUTITLE", "Title in menu");
+define("_CO_IMFAQ_CATEGORY_CAT_MENUTITLE", "Titolo nel menu");
 define("_CO_IMFAQ_CATEGORY_CAT_MENUTITLE_DSC", " ");
-define("_CO_IMFAQ_CATEGORY_CAT_PID", "Parent category");
-define("_CO_IMFAQ_CATEGORY_CAT_PID_DSC", "If you want this new category to be a sub-category, select a parent here. If it is to be a top level category, leave this blank.");
+define("_CO_IMFAQ_CATEGORY_CAT_PID", "Categoria padre");
+define("_CO_IMFAQ_CATEGORY_CAT_PID_DSC", "Se vuoi che questa nuova categoria sia una sottocategoria, seleziona qui un genitore. Se deve essere una categoria di primo livello, lasciare questo vuoto.");
 define("_CO_IMFAQ_CATEGORY_CAT_SUMMARY", "Summary");
 define("_CO_IMFAQ_CATEGORY_CAT_SUMMARY_DSC", " ");
 define("_CO_IMFAQ_CATEGORY_CAT_DESCRIPTION", "Descrizione");
 define("_CO_IMFAQ_CATEGORY_CAT_DESCRIPTION_DSC", " ");
-define("_CO_IMFAQ_CATEGORY_CAT_WEIGHT", "Weight");
+define("_CO_IMFAQ_CATEGORY_CAT_WEIGHT", "Peso");
 define("_CO_IMFAQ_CATEGORY_CAT_WEIGHT_DSC", " ");
-define("_CO_IMFAQ_CATEGORY_CAT_PUBLISHED_DATE", "Published Date");
+define("_CO_IMFAQ_CATEGORY_CAT_PUBLISHED_DATE", "Data Pubblicazione");
 define("_CO_IMFAQ_CATEGORY_CAT_PUBLISHED_DATE_DSC", " ");
 define("_CO_IMFAQ_CATEGORY_CAT_STATUS", "Stato");
 define("_CO_IMFAQ_CATEGORY_CAT_STATUS_DSC", " ");
-define("_CO_IMFAQ_CATEGORY_CAT_SUBS", "SubCategories");
+define("_CO_IMFAQ_CATEGORY_CAT_SUBS", "Sottocategorie");
 define("_CO_IMFAQ_CATEGORY_CAT_SUBS_DSC", " ");
 
 define("_CO_IMFAQ_CATEGORY_STATUS_ACTIVE", "Attivo");
 define("_CO_IMFAQ_CATEGORY_STATUS_INACTIVE", "Non attivo");
 
-define("_CO_IMFAQ_CATEGORY_READ", "View Permission");
-define("_CO_IMFAQ_CATEGORY_READ_DSC", "Select wich groups will have view permission for this category. This means that a user belonging to one of these groups will be able to view the category when it is activated in the site.");
-define("_CO_IMFAQ_CATEGORY_WRITE", "Write Permission");
-define("_CO_IMFAQ_CATEGORY_WRITE_DSC", "Select the groups which are allowed to create new faqs on this category. This means that a user belonging to one of these groups will be able to add new faqs in this category directly on the site.");
+define("_CO_IMFAQ_CATEGORY_READ", "Visualizza Permesso");
+define("_CO_IMFAQ_CATEGORY_READ_DSC", "Seleziona i gruppi wich avranno i permessi di visualizzazione per questa categoria. Ciò significa che un utente appartenente a uno di questi gruppi sarà in grado di visualizzare la categoria quando viene attivato nel sito.");
+define("_CO_IMFAQ_CATEGORY_WRITE", "Permesso Di Scrittura");
+define("_CO_IMFAQ_CATEGORY_WRITE_DSC", "Seleziona i gruppi che possono creare nuovi faqs in questa categoria. Ciò significa che un utente appartenente a uno di questi gruppi sarà in grado di aggiungere nuovi faq in questa categoria direttamente sul sito.");
 
 // Attachments
 define("_CO_IMFAQ_ATTACHMENT_ATTACH_ID", "Id");
@@ -95,20 +95,20 @@ define("_CO_IMFAQ_ATTACHMENT_ATTACH_FID", "Faq");
 define("_CO_IMFAQ_ATTACHMENT_ATTACH_FID_DSC", " ");
 define("_CO_IMFAQ_ATTACHMENT_ATTACH_MODULE", "Modulo");
 define("_CO_IMFAQ_ATTACHMENT_ATTACH_MODULE_DSC", " ");
-define("_CO_IMFAQ_ATTACHMENT_ATTACH_ITEMID", "Item");
+define("_CO_IMFAQ_ATTACHMENT_ATTACH_ITEMID", "Elemento");
 define("_CO_IMFAQ_ATTACHMENT_ATTACH_ITEMID_DSC", " ");
-define("_CO_IMFAQ_ATTACHMENT_ATTACH_WEIGHT", "Weight");
+define("_CO_IMFAQ_ATTACHMENT_ATTACH_WEIGHT", "Peso");
 define("_CO_IMFAQ_ATTACHMENT_ATTACH_WEIGHT_DSC", " ");
 define("_CO_IMFAQ_ATTACHMENT_ATTACH_STATUS", "Stato");
 define("_CO_IMFAQ_ATTACHMENT_ATTACH_STATUS_DSC", " ");
 
-define("_CO_IMFAQ_ATTACHMENT_STATUS_ACTIVE", "Show");
-define("_CO_IMFAQ_ATTACHMENT_STATUS_INACTIVE", "Hide");
+define("_CO_IMFAQ_ATTACHMENT_STATUS_ACTIVE", "Mostra");
+define("_CO_IMFAQ_ATTACHMENT_STATUS_INACTIVE", "Nascondi");
 
-define("_CO_IMFAQ_ATTACHMENT_READ", "View Permission");
-define("_CO_IMFAQ_ATTACHMENT_READ_DSC", "Select wich groups will have view permission for this attachment. This means that a user belonging to one of these groups will be able to view the attachment when it is activated in the site.");
+define("_CO_IMFAQ_ATTACHMENT_READ", "Visualizza Permesso");
+define("_CO_IMFAQ_ATTACHMENT_READ_DSC", "Seleziona i gruppi wich avranno i permessi di visualizzazione per questo allegato. Ciò significa che un utente appartenente a uno di questi gruppi sarà in grado di visualizzare l'allegato quando viene attivato nel sito.");
 
-define("_CO_IMFAQ_ATTACHMENT_SEL_MODULE", "Select a module plugin: ");
-define("_CO_IMFAQ_ATTACHMENT_AVALIABLE_ITEMS", "Avaliable Items from %s");
-define("_CO_IMFAQ_ATTACHMENT_AVALIABLE_ITEMS_DSC", "Bellow is a list of all avaliable items for the selected module. Select the desired items and click on the add button to attach it in the FAQ.");
+define("_CO_IMFAQ_ATTACHMENT_SEL_MODULE", "Seleziona un plugin del modulo: ");
+define("_CO_IMFAQ_ATTACHMENT_AVALIABLE_ITEMS", "Oggetti preziosi da %s");
+define("_CO_IMFAQ_ATTACHMENT_AVALIABLE_ITEMS_DSC", "Bellow è un elenco di tutti gli elementi disponibili per il modulo selezionato. Selezionare gli elementi desiderati e fare clic sul pulsante Aggiungi per attaccarlo nelle FAQ.");
 ?>
