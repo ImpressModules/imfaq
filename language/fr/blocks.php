7de3249895d8021736fc6ebfc558f905a7aabc9f
David Janssens <fiammybe@gmail.com>
2025-03-19 11:41:49 +0100

    New translations blocks.php (French)

--- language/fr/blocks.php
+++ language/fr/blocks.php
@@ -12,20 +12,20 @@
 
 if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
 
-define("_MB_IMFAQ_CATLIST_SHOWSUBS", "Show subcategories:");
-define("_MB_IMFAQ_CATLIST_SORT", "Sort by:");
+define("_MB_IMFAQ_CATLIST_SHOWSUBS", "Afficher les sous-catégories :");
+define("_MB_IMFAQ_CATLIST_SORT", "Trier par:");
 define("_MB_IMFAQ_CATLIST_ORDER", "Ordre d'affichage dans le gestionnaire d'images:");
 
-define("_MB_IMFAQ_DIDUNO_LIMIT", "Limit:");
+define("_MB_IMFAQ_DIDUNO_LIMIT", "Limite :");
 define("_MB_IMFAQ_DIDUNO_CATEG", "Cat&eacute;gorie :");
-define("_MB_IMFAQ_ORDERBYRAND", "Random");
-define("_MB_IMFAQ_ALLCATEGS", "All Categories");
-define("_MB_IMFAQ_ONLYCURRENTCATEG", "For current category only");
+define("_MB_IMFAQ_ORDERBYRAND", "Aléatoire");
+define("_MB_IMFAQ_ALLCATEGS", "Toutes les catégories");
+define("_MB_IMFAQ_ONLYCURRENTCATEG", "Pour la catégorie courante seulement");
 
-define("_MB_IMFAQ_CATEGORY_CAT_WEIGHT", "Weight");
+define("_MB_IMFAQ_CATEGORY_CAT_WEIGHT", "Poids");
 define("_MB_IMFAQ_CATEGORY_CAT_ID", "Id");
 define("_MB_IMFAQ_CATEGORY_CAT_TITLE", "Titre");
 define("_MB_IMFAQ_FAQ_FAQ_QUESTION", "Question");
-define("_MB_IMFAQ_FAQ_FAQ_WEIGHT", "Weight");
+define("_MB_IMFAQ_FAQ_FAQ_WEIGHT", "Poids");
 define("_MB_IMFAQ_FAQ_FAQ_ID", "Id");
 ?>
