e5ec144ce8d95a515a8850c65ae6fe88cb450eb3
David Janssens <fiammybe@gmail.com>
2025-03-19 10:44:40 +0100

    New translations blocks.php (Dutch)

---
+++ language/nl/blocks.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* English language constants used in blocks of the module
+*
+* @copyright	INBOX International
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		Rodrigo Pereira Lima <rodrigo@inboxinternational.com>
+* @package		imfaq
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+define("_MB_IMFAQ_CATLIST_SHOWSUBS", "Show subcategories:");
+define("_MB_IMFAQ_CATLIST_SORT", "Sort by:");
+define("_MB_IMFAQ_CATLIST_ORDER", "Toon volgorde in afb. manager:");
+
+define("_MB_IMFAQ_DIDUNO_LIMIT", "Limit:");
+define("_MB_IMFAQ_DIDUNO_CATEG", "Categorie:");
+define("_MB_IMFAQ_ORDERBYRAND", "Random");
+define("_MB_IMFAQ_ALLCATEGS", "All Categories");
+define("_MB_IMFAQ_ONLYCURRENTCATEG", "For current category only");
+
+define("_MB_IMFAQ_CATEGORY_CAT_WEIGHT", "Weight");
+define("_MB_IMFAQ_CATEGORY_CAT_ID", "Id");
+define("_MB_IMFAQ_CATEGORY_CAT_TITLE", "Titel");
+define("_MB_IMFAQ_FAQ_FAQ_QUESTION", "Question");
+define("_MB_IMFAQ_FAQ_FAQ_WEIGHT", "Weight");
+define("_MB_IMFAQ_FAQ_FAQ_ID", "Id");
+?>
