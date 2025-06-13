aafafa2ef21de6de9a3176cd67582c4a4eb5f790
David Janssens <fiammybe@gmail.com>
2025-06-13 10:43:13 +0200

    New translations main.php (Serbian (Cyrillic))

---
+++ language/sr/main.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* English language constants used in the user side of the module
+*
+* @copyright	INBOX International
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		Rodrigo Pereira Lima <rodrigo@inboxinternational.com>
+* @package		imfaq
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+
+define('_MD_IMFAQ_ADMIN_PAGE', ':: Admin page ::');
+
+define("_MD_IMFAQ_ALL_FAQS", "All faqs");
+define("_MD_IMFAQ_ALL_CATEGORYS", "All categorys");
+
+define('_MD_IMFAQ_FAQ_EDIT', 'Edit FAQ');
+define('_MD_IMFAQ_FAQ_SUBMIT', 'Submit a new FAQ');
+define('_MD_IMFAQ_FAQ_REQUEST', 'Request an answer');
+define('_MD_IMFAQ_FAQ_MODIFIED', 'The FAQ was successfully modified.');
+define('_MD_IMFAQ_FAQ_CREATED', 'The FAQ has been successfully created.');
+define('_MD_IMFAQ_SECURITY_CHECK_FAILED', 'Security check failed...');
+
+define('_MD_IMFAQ_RSS_GLOBAL', 'Subscribe to latest FAQs');
+define('_MD_IMFAQ_RSS_CATEGORY', 'Subscribe to latest FAQs from this category');
+define('_MD_IMFAQ_RSS_POSTER', 'Subscribe to latest FAQs from this poster');
+
+define('_MD_IMFAQ_NOFAQSELECTED','You did not select a valid FAQ!')
+?>
